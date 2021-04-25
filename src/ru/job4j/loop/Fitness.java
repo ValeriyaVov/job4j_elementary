12bb07a09c7c95f1a22a5f1c76b4a8e8c47d26d7
Lera <valeriavovnenko0@gmail.com>
2021-04-25 13:03:55 +0300

    5.4. Протеиновая диета [#489925]

---
+++ src/ru/job4j/loop/Fitness.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan > nik) {
+            System.out.println(month);
+             ivan *= 3;
+        }
+        return month;
+    }
+}
