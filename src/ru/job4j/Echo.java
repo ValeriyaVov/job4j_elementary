78e5d39d192f4d1ac86163bac179452096f2eb7f
Lera <valeriavovnenko0@gmail.com>
2021-05-26 15:46:54 +0300

    5.6. Простые числа [#489926]

--- src/ru/job4j/Echo.java
+++ src/ru/job4j/Echo.java
@@ -1,4 +1,13 @@
 package ru.job4j;
 
 public class Echo {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++b);
+        System.out.println(b);
+
+    }
 }
