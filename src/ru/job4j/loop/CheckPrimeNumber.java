c953109b47dd1682a819123e277cff9f53a7fd9b
Lera <valeriavovnenko0@gmail.com>
2021-06-14 13:05:31 +0300

    5.7. Ипотека [#156315 #167500]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,13 +2,13 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = number > 1;
-        for (int i = 2; i < number; i++) {
-            if ((number % i) == 0) {
-                prime = false;
-                break;
+        boolean prime = number > 1; //Логическая переменная со !!!!значением!!! true или false (больше или меньше 1)
+        for (int i = 2; i < number; i++) { // Если здесь условие не соблюдаются, то идем на 12 строчку
+            if ((number % i) == 0) { // % Это нахождение остатка от деления одного целого числа на другое // Если здесь условие не соблюдаются, то идем на 12 строчку
+                prime = false; // Если условия строчкой выше соблюдены, то в переменную prime записывается значение false
+                break; // прерывание цикла
             }
         }
-        return prime;
+        return prime; //Здесь возвращается значение переменной prime
     }
 }
