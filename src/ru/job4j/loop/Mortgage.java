962d82e2dfe8d5907b6aced5f33408e34636f2d1
Lera <valeriavovnenko0@gmail.com>
2021-06-21 20:13:02 +0300

    5.9.1. Генерация конструкций в IDEA [#279134]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -4,8 +4,8 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            amount = amount + (amount * percent / 100);
-            amount -= salary;
+            double yearDebt = amount + (amount * percent / 100);
+            amount = yearDebt - salary;
             year++;
         }
         return year;
